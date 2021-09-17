6c13dcbd76fdbe561bbdd6696a3ea5bbc1e65c39
xlan <zhuiming.mu@gmail.com>
2021-09-17 16:22:59 +0800

    update

---
+++ src/android/TencentYSDKPlugin.java
@@ -0,0 +1,73 @@
+package cordova.plugin.ysdk;
+
+import android.content.Intent;
+import android.util.Log;
+
+import com.tencent.ysdk.api.YSDKApi;
+import com.tencent.ysdk.framework.common.BaseRet;
+import com.tencent.ysdk.module.user.UserListener;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.tencent.ysdk.framework.common.ePlatform;
+import com.tencent.ysdk.module.user.UserLoginRet;
+import com.tencent.ysdk.module.user.UserRelationRet;
+import com.tencent.ysdk.module.user.WakeupRet;
+
+public class TencentYSDKPlugin extends CordovaPlugin {
+  private static final String TAG = "LoginActivity";
+
+  @Override
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+    super.initialize(cordova, webView);
+    YSDKApi.init();
+  }
+
+  @Override
+  public boolean execute(
+          String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+
+    JSONObject jsonObject = args.getJSONObject(0);
+    // 展示开屏广告
+    if ("initYSDK".equals(action)) {
+
+      YSDKApi.setUserListener(new UserListener() {
+        @Override
+        public void OnLoginNotify(UserLoginRet userLoginRet) {
+          SendCallback(callbackContext,"onLogin",new JSONObject());
+        }
+
+        @Override
+        public void OnWakeupNotify(WakeupRet wakeupRet) {
+          SendCallback(callbackContext,"onWakeup",new JSONObject());
+        }
+
+        @Override
+        public void OnRelationNotify(UserRelationRet userRelationRet) {
+          SendCallback(callbackContext,"onRelation",new JSONObject());
+        }
+      });
+      YSDKApi.login(ePlatform.Guest);
+      return true;
+    }
+    return false;
+  }
+
+  private void SendCallback(CallbackContext callbackContext, String event, JSONObject param) {
+    PluginResult result = new PluginResult(PluginResult.Status.OK, param);
+    result.setKeepCallback(true);
+    try {
+      param.put("type", event);
+    }catch (Exception e) {
+    }
+    callbackContext.sendPluginResult(result);
+  }
+
+}
---
+++ www/device.js
@@ -0,0 +1,47 @@
+var exec = require('cordova/exec');
+var channel = require('cordova/channel');
+
+
+/**
+ * 事件说明
+ * onLogin     
+ * onWakeup
+ * onRelation
+ */
+
+var Base = function() {
+    this.channels = {};
+};
+
+Base.prototype = {
+    _eventHandler: function(event) {
+      if (event && (event.type in this.channels)) {
+          this.channels[event.type].fire(event);
+      }
+    },
+    on: function(type, func) {
+        if (!(type in this.channels)) {
+            this.channels[type] = channel.create(type);
+        }
+        this.channels[type].subscribe(func);
+        return this;
+    },
+};
+
+var YSDK = function() { Base.call(this); };
+YSDK.prototype = new Base();
+
+YSDK.init = function() {
+  var options = {};
+  var base = new YSDK();
+  var cb = function(eventname) {
+    base._eventHandler(eventname);
+  };
+  exec(cb, cb, 'TencentYSDKPlugin', 'initYSDK', [options]);
+  return base;
+}
+
+
+module.exports = {
+    YSDK: YSDK
+};
